f4d65e702087cd69fb96867a69990c406d30ac2b
Si <si@medatechuk.com>
2023-04-12 10:16:45 +0100

    Bank Holiday

--- REPLACEVENDOR.sql
+++ REPLACEVENDOR.sql
@@ -20,3 +20,23 @@ update [system].[dbo].[COLUMNS]
 set TITLE = REPLACE(TITLE, 'Vendor' , 'Suppl.')
 WHERE TITLE LIKE '%Vendor%'
 AND LEN(REPLACE(TITLE, 'Vendor' , 'Supplier')) >= 21
+
+update [system].[dbo].[EXTMSG]
+set MESSAGE = REPLACE(MESSAGE, 'Vendor' , 'Supplier')
+WHERE MESSAGE LIKE '%Vendor%'
+AND LEN(REPLACE(MESSAGE, 'Vendor' , 'Supplier')) < 57
+
+update [system].[dbo].[EXTMSG]
+set MESSAGE = REPLACE(MESSAGE, 'Vendor' , 'Suppl.')
+WHERE MESSAGE LIKE '%Vendor%'
+AND LEN(REPLACE(MESSAGE, 'Vendor' , 'Supplier')) >= 57
+
+update [system].[dbo].[LANGEXEC]
+set TITLE = REPLACE(TITLE, 'Vendor' , 'Supplier')
+WHERE TITLE LIKE '%Vendor%'
+AND LEN(REPLACE(TITLE, 'Vendor' , 'Supplier')) < 65
+
+update [system].[dbo].[LANGEXEC]
+set TITLE = REPLACE(TITLE, 'Vendor' , 'Suppl.')
+WHERE TITLE LIKE '%Vendor%'
+AND LEN(REPLACE(TITLE, 'Vendor' , 'Supplier')) >= 65
